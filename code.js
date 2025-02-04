e7239c1c83214a54eb3d4afec06c0674fc3dd96f
Kane Kriz <kanemkriz@gmail.com>
2025-02-04 11:14:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -16,31 +16,32 @@ function permutationSort(a)
     var listLen = a.length;
 
     //
-
-
-
+    
     //check for already sorted list
     //...added based on error logs from [0,1] not working but it also might just be that the code is bad enough that the first
     // randomly generated or intentionally provided "a" list from the test code already breaks.
+    
     //
     
     if(a.length == 0 || a.length == 1)
     {
-        return 0;
+        return 0; //0 iterations for either of these examples
     }
     for(var i = 1; i < a.length; i++)
     {
         if(a[i] < a[i-1])
         {
-            break;
+            break; 
+        }
+        else //added this else as when it was just the "if" it wasn't accomplishing anything?
+        {
+            return 0;
         }
     }
 
-
     //
-
     
-    originalList = a.slice();
+    originalList = a.slice(); //copy original list if I am understanding slice() correctly
     var numIterations = 0;
     
     while(numIterations < listLen) //shouldve just used a for. Guess i'll roll with this
@@ -53,14 +54,15 @@ function permutationSort(a)
     {
         if(bruteForceList[i] < bruteForceList[i-1]) 
         {
-            return numPerms + permutationSort(a);
+            return numPerms + permutationSort(a); //"numPerms +" necessary?
         }
     }
 
     
     for (var i = 0; i<listLen; i++) 
     {
-        a[i] = originalList[bruteForceList[i]]; //this is awful to look at
+        //this is awful to look at. Probably not even right
+       // a[i] = originalList[bruteForceList[i]]; 
     }
 
     //
