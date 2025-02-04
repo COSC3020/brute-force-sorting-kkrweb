3b954a4551255945506f3f92ef625b7a51776410
Kane Kriz <kanemkriz@gmail.com>
2025-02-04 10:15:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
-function permutationSort(a) {
-    return 0;
+// Kane Kriz
+// UWYO COSC 3020
+// 4 Feb 2025
+//
+//
+
+
+
+function permutationSort(a)
+{
+    var numPerms = 0;
+    var bruteForceList = [];
+    var originalList = [];
+    var listLen = a.length;
+
+    //
+
+    originalList = a.slice();
+    var numIterations = 0;
+    
+    while(numIterations < listLen) //shouldve just used a for. Guess i'll roll with this
+    {
+        bruteForceList.push(Math.floor(Math.random() * listLen)); //this might just be completely useless
+        numIterations++; 
+    }
+    
+    for(let i = 1; i < bruteForceList.length; i++) 
+    {
+        if(bruteForceList[i] < bruteForceList[i-1]) 
+        {
+            numIterations = 0;
+            bruteForceList = []; //reset this so it can be randomized again...maybe not necessary?
+            numPerms++;
+            return permutationSort(a);
+        }
+    }
+
+    //
+    
+    return numPerms; // number of permutation attempts
 }
