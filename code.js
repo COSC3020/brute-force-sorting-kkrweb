a08968619f5dcfc031d401eaf072b3cbe86e1538
Kane Kriz <kanemkriz@gmail.com>
2025-03-19 14:19:36 -0500

    Update code.js

--- code.js
+++ code.js
@@ -5,8 +5,7 @@
 //
 
 
-
-
+//
 
 
 function permutationSort(initialArray)
@@ -42,10 +41,17 @@ function permutationSort(initialArray)
         return true;
     }
 
+    //following implementation inspired with help from the GeeksforGeeks cited
+    //
+    // I had no problem invisioning this implementation for the longest time and attempting to implement it
+    //but the issues would arise when I would attempt to make the sort have actual systematic progress
+    // Considering the GeeksforGeeks "next permutation" helped me fix the logical issues with my code and actual had it go somewhere ha
+
     //function to generate the next permutation systematticaly
     function nextPermutation(createdArray)
     {
         var i = createdArray.length - 2;
+        
         while (i >= 0 && createdArray[i] >= createdArray[i + 1])
         {
             i--;
@@ -58,15 +64,17 @@ function permutationSort(initialArray)
             {
                 j--;
             }
-            // Swap createdArray[i] and createdArray[j]
+            
+            //swapping createdArray[i] and createdArray[j]
             temp = createdArray[i];
             createdArray[i] = createdArray[j];
             createdArray[j] = temp;
         }
 
-        //cite this
+        
         var left = i + 1;
         var right = createdArray.length - 1;
+        
         while (left < right)
         {
             temp = createdArray[left];
@@ -98,126 +106,4 @@ function permutationSort(initialArray)
     return numPerms; //returns number of perms as specified within the directions
 }
 
-
-
-
-//___________________________________________________________________________________________________________________________________________________
-/*
-STARTING THIS WHOLE EXERCISE OVER
-My code is too bad, will remain below in this block.
-
-// this is broken junk code in its current state
-// either stack overflows or fails with simple arrays
-// work in progress ha
-
-
-// This is Ship of Theseus code...
-// I have nuked it enough times that I don't know if it even deserves to live in the same file anymore
-
-
 //
-// Seems like recursion on this is just asking for a stack overflow
-//options are, a stack overflow via recursion with pretty reasonable to follow logic
-//or iterative, extremely complicated logic.
-//
-
-function permutationSort(a)
-{
-    //idea is to create an array via checkedIndices to help eliminate duplicate sorting
-    //i.e. of a 3 element array, it would try 0 1 2 then 0 2 1, then recording that all permutations with original index 0 still in slot 0 are not sorted.
-    //then it would check 1 0 2 and 1 2 0, still seaching for a sorted array via swapping around the indices
-    
-    var numPerms = 0; 
-    var bruteForceList = [];
-    var originalList = [];
-    var checkedIndices = []; //list to keep track of checked first position indices
-    var listLen = a.length;
-
-    //checking for an already sorted list, guaranteed with array entries of size 0 or 1.
-    if(a.length == 0 || a.length == 1)
-    {
-        return 0; //0 iterations for either of these examples
-    }
-
-    for(var i = 0; i < a.length - 1; i++) 
-    {
-        if(a[i] > a[i+1])
-        {
-            break;
-        }
-        if(i == a.length - 2)
-        {
-            return 0;
-        }
-    }
-
-    originalList = a.slice(); //copy original list if I am understanding slice() correctly
-
-    var numIterations = 0; //num of permutation attempts as specified within the directions. Glorified for loop with this heading below? Yeah
-    //This probably could have been not used in favor of using numPerms again somehow
-
-    var currentIndex = 0;
-
-    while(checkedIndices.length < listLen) //loop until all indices are checked
-    {
-        var duplicateBool = false; 
-        //bool for whether or not a duplicate entry for the permutation is generated...
-        //this is probably very cumbersome and inefficient, but if it works it works I suppose...
-
-        if(!checkedIndices.includes(currentIndex)) 
-        {
-            var tempOriginalList = originalList.slice(); //gross
-
-            while(tempOriginalList.length > 0)
-            {
-                for(var j = 0; j < bruteForceList.length; j++) 
-                {
-                    if(bruteForceList[j] == tempOriginalList[j]) 
-                    {
-                        duplicateBool = true;
-                        break;
-                    }
-                }
-
-                if(!duplicateBool && tempOriginalList.length > 0) 
-                {
-                    bruteForceList.push(tempOriginalList.shift()); 
-                    numIterations++;
-                }
-
-                for(var i = 0; i < bruteForceList.length - 1; i++)
-                {
-                    if(bruteForceList[i] > bruteForceList[i+1])
-                    {
-                        bruteForceList = [];
-                        originalList = a.slice();
-                        numPerms++;
-                        duplicateBool = false;
-                        break;
-                    }
-                }
-
-                if(!duplicateBool)
-                {
-                    break;
-                }
-            }
-            checkedIndices.push(currentIndex); //adding index to checked list only after all permutations are checked
-        }
-        currentIndex++;
-        if(currentIndex >= listLen)
-        {
-            currentIndex = 0;
-        }
-    }
-
-    for(var i = 0; i < listLen; i++)
-    //is this for even necessary? Is altering the input array even necessary to complete this exercise?
-    //seems unnecessary. I'm going to add and remove it and see if anything changes with the test code running
-    {
-        a[i] = bruteForceList[i]; //this is awful to look at. Probably not even right
-    }
-    return numPerms; //returns number of permutations as specified within the problem
-}
-
-*/
