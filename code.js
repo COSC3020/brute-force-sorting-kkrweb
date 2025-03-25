1d5c51a3d457eb763d456104e414de24cfdde6e3
Kane Kriz <kanemkriz@gmail.com>
2025-03-24 18:06:20 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // Kane Kriz
 // UWYO COSC 3020
-// 19 March 2025
-//
+// Brute Force Sorting Exercise
+// 24 March 2025
 //
 
 
@@ -23,7 +23,7 @@ function permutationSort(initialArray)
     var endPos; 
 
     //checking for an already sorted list, guaranteed with array entries of size 0 or 1.
-    if (arrLen == 0 || arrLen == 1)
+    if(arrLen == 0 || arrLen == 1)
     {
         return 0; //0 iterations for either of these examples
     }
@@ -31,9 +31,9 @@ function permutationSort(initialArray)
     //checking if array is sorted
     function sortCheck(createdArray)
     {
-        for (var i = 1; i < createdArray.length; i++)
+        for(var i = 1; i < createdArray.length; i++)
         {
-            if (createdArray[i - 1] > createdArray[i])
+            if(createdArray[i - 1] > createdArray[i])
             {
                 return false;
             }
@@ -41,18 +41,18 @@ function permutationSort(initialArray)
         return true;
     }
 
-    //following implementation inspired with help from the GeeksforGeeks cited
-    //
+    // Following logical implementation was completed with help from the GeeksforGeeks cited.
     // I had no problem invisioning this implementation for the longest time and attempting to implement it
-    //but the issues would arise when I would attempt to make the sort have actual systematic progress
-    // Considering the GeeksforGeeks "next permutation" helped me fix the logical issues with my code and actual had it go somewhere ha
+    //but the issues would arise when I would attempt to make the sort have actual systematic progress.
+    // Considering the GeeksforGeeks "next permutation" helped me fix the logical issues with my code and actual had it go somewhere ha.
+    // Several versions became glorified random non-memory permutation checkers due to me royally messing up the logic for a while.
 
     //function to generate the next permutation systematticaly
     function nextPermutation(createdArray)
     {
         var i = createdArray.length - 2;
         
-        while (i >= 0 && createdArray[i] >= createdArray[i + 1])
+        while(i >= 0 && createdArray[i] >= createdArray[i + 1])
         {
             i--;
         }
@@ -60,7 +60,8 @@ function permutationSort(initialArray)
         if(i >= 0)
         {
             var j = createdArray.length - 1;
-            while (createdArray[j] <= createdArray[i])
+            
+            while(createdArray[j] <= createdArray[i])
             {
                 j--;
             }
@@ -75,7 +76,7 @@ function permutationSort(initialArray)
         var left = i + 1;
         var right = createdArray.length - 1;
         
-        while (left < right)
+        while(left < right)
         {
             temp = createdArray[left];
             createdArray[left] = createdArray[right];
@@ -86,18 +87,18 @@ function permutationSort(initialArray)
     }
 
     //check starting array to see if it is already sorted before permutations
-    if (sortCheck(initialArray))
+    if(sortCheck(initialArray))
     {
         return 0; //already sorted, no permutations necessary
     }
 
     //generating permutations systematically until the sorted one is found and sortedPerm becomes true
-    while (!sortedPerm)
+    while(!sortedPerm)
     {
         nextPermutation(initialArray);
         numPerms++;
         
-        if (sortCheck(initialArray))
+        if(sortCheck(initialArray))
         {
             sortedPerm = true;
         }
